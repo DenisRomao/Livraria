b0f197a37adcb62b034ad9674644fc35643c33d4
Denis Romao <de.silva.romao@gmail.com>
2022-12-19 14:59:45 -0300

    Iniciando projeto Livraria

---
+++ src/CalculadoraDeEstoque.java
@@ -0,0 +1,11 @@
+public class CalculadoraDeEstoque {
+    public static void main(String[] args) {
+        double livroJava8;
+        double LivroTDD;
+
+        livroJava8 = 59.90;
+        LivroTDD = 59.90;
+
+        System.out.println(livroJava8);
+    }
+}
